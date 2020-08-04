63938daf720b4500338ac46f2639bc3805561c91
Amlesh Sivanantham (zamlz) <zamlz@pm.me>
2020-08-04 10:13:02 -0700

    Updated conversion script

--- scripts/wiki2md.sh
+++ scripts/wiki2md.sh
@@ -4,6 +4,21 @@
 # things that are useful to have in vimwiki but are not ideal in actual
 # markdown.
 
+# perl script cleans up links found within wiki files. While the work in
+# vimwiki, once converted to html, the links are broken. If the link doesn't
+# begin with http, we are pretty sure its a wiki link so we make sure to
+# append .html to the link
 
-cat $1 | perl -ne 's/(\[.+\])\(((?!http)[^)]+)\)/\1(\2.html)/g; print;'
+# Using sed, we must convert vimwiki checkboxes to valid markdown checkboxes
+# so it can be properly converted by pandoc into html. My vim config configures
+# checkboxes as such:
+#   let g:vimwiki_listsyms = ' ○◐●✓'
+
+cat $1 \
+    | perl -ne 's/(\[.+\])\(((?!http)[^)]+)\)/\1(\2.html)/g; print;' \
+    | sed -e 's/\[○\]/\[ \]/g' \
+          -e 's/\[◐\]/\[ \]/g' \
+          -e 's/\[●\]/\[ \]/g' \
+          -e 's/\[✓\]/\[X\]/g'
+
 
